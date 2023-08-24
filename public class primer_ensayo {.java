e8722062ee46899e56fc03b7814d848fee5a5e3d
brandoncerru <142032619+brandoncerru@users.noreply.github.com>
2023-08-24 07:38:49 -0500

    Add files via upload

---
+++ public class primer_ensayo {.java
@@ -0,0 +1,17 @@
+public class primer_ensayo {
+ 
+    public static void main (String[] args) {
+        double a = 5.5, b = 4;   
+        double raiz = Math.sqrt(a);
+        System.out.println("La raiz es: " + raiz);
+        
+        long redondo = Math.round(a);
+        System.out.println("El numero redondo es: " + redondo);
+
+        double c = 5, d = 3;
+        double elevado = Math.pow(c, d);
+        System.out.println("El numero elevado es: " + elevado);
+    }
+}
+
+
